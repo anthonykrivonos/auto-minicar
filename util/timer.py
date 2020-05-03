8d6523835cc4dbf86d92e83f9d8cb490f29ce57d
Anthony Krivonos <abkrivonos@gmail.com>
2020-05-03 12:39:54 -0400

    Removed Timer autostart

--- util/timer.py
+++ util/timer.py
@@ -15,7 +15,6 @@ class Timer(object):
         self.args       = args
         self.kwargs     = kwargs
         self.is_running = False
-        self.start()
 
     def _run(self):
         self.is_running = False
